0b7e2c5333cba74b74d88881cfd82e72b55eab0a
biletskakatia <152877295+biletskakatia@users.noreply.github.com>
2024-04-12 16:12:26 +0300

    3

--- js/task-6.js
+++ js/task-6.js
@@ -11,7 +11,6 @@ const input = document.querySelector('input[type="number"]');
   createBtn.addEventListener('click', () => {
       const amount = parseInt(input.value);
     if (amount >= 1 && amount <= 100) {
-        destroyBoxes();
         createBoxes(amount);
         input.value = '';
       } else {
@@ -22,7 +21,7 @@ const input = document.querySelector('input[type="number"]');
 destroyBtn.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
-
+  destroyBoxes();
   for (let i = 0; i < amount; i++) {
     const size = 30 + i * 10;
     const div = document.createElement('div');
